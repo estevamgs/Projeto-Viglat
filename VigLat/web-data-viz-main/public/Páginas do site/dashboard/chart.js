03210f35d563e5cc957953e17ae6b9aa75894eee
ViniciusGarban <vinicius.garban@sptech.school>
2026-04-21 17:59:42 -0300

    Adicionado outro gráfico de alerta
    Foi divido o grafico de alerta em 2, um de alerta das ultimas 24 horas, e outro de alertas semanais

--- VigLat/web-data-viz-main/public/Páginas do site/dashboard/chart.js
+++ VigLat/web-data-viz-main/public/Páginas do site/dashboard/chart.js
@@ -1,4 +1,5 @@
 const alertas = document.getElementById("chart");
+const alertaSemana = document.getElementById("grafico-alerta");
 const graficoTemp = document.getElementById("grafico-temp");
 const graficoUmidade = document.getElementById("grafico-umidade");
 
@@ -6,20 +7,64 @@ new Chart(alertas, {
   type: "bar",
   data: {
     labels: [
-      "Segunda feira",
-      "Terça Feira",
-      "Quarta Feira",
-      "Quinta Feira",
-      "Sexta Feira",
-      "Sábado",
-      "Domingo",
+      "CÂMARA 01",
+      "CÂMARA 02",
+      "CÂMARA 03",
+      "CÂMARA 04",
+      
     ],
     datasets: [
       {
         label: "Alertas",
-        data: [1, 3, 1, 0, 5, 3, 2],
-        borderColor: "#CD1C18",
-        backgroundColor: "#CD1C18",
+        data: [0,1,0,4],
+        borderColor: "#7a5208",
+        backgroundColor: "#7a5208",
+      },
+    ],
+  },
+  options: {
+    maintainAspectRatio: false,
+    plugins: {
+      title: {
+        display: true,
+        text: "Alertas das ultimas 24 horas",
+      },
+    },
+    scales: {
+      y: {
+        stacked: true,
+        grid: {
+          display: true,
+          color: "rgba(255,99,132,0.2)",
+        },
+      },
+      x: {
+        grid: {
+          display: false,
+        },
+      },
+    },
+  },
+});
+new Chart(alertaSemana, {
+  type: "bar",
+  data: {
+    labels: [
+      "Segunda-Feira",
+      "Terça-Feira",
+      "Quarta-Feira",
+      "Quinta-Feira",
+      "Sexta-Feira",
+      "Sabado",
+      "Domingo",
+      
+    ],
+    datasets: [
+      {
+        label: "Total de Alertas Semanais",
+        data: [1,1,0,3,4,1,1],
+        borderColor: "#7a5208",
+        backgroundColor: "#7a5208",
       },
     ],
   },
@@ -49,31 +94,31 @@ new Chart(alertas, {
 });
 
 const dadosSensorA = {
-  horas: ["12:02", "12:04", "12:06", "12:08"],
-  temperatura: [25, 21, 20, 22],
-  umidade: [80, 82, 80, 85],
+  horas: ["12:00", "13:00", "14:00", "15:00","16:00","17:00","18:00","19:00"],
+  temperatura: [22, 21, 20, 22,22, 21, 20, 22],
+  umidade: [80, 82, 80, 85,80, 82, 80, 85],
 };
 
 const dadosSensorB = {
   horas: dadosSensorA.horas,
-  temperatura: [15, 27, 26, 32],
-  umidade: [60, 67, 72, 65],
+  temperatura: [20, 20, 26, 22,15, 27, 26, 32],
+  umidade: [60, 67, 72, 65,60, 67, 72, 85],
 };
 
 const dadosSensorC = {
   horas: dadosSensorA.horas,
-  temperatura: [18, 22, 20, 21],
-  umidade: [97, 87, 90, 80],
+  temperatura: [18, 22, 20, 21,18, 22, 20, 21],
+  umidade: [90, 87, 90, 80,80, 87, 90, 80],
 };
 const tempMaxIdeal = {
   horas: dadosSensorA.horas,
-  temperatura: [22, 22, 22, 22],
-  umidade: [95, 95, 95, 95],
+  temperatura: [22, 22, 22, 22,22, 22, 22, 22],
+  umidade: [95, 95, 95, 95,95, 95, 95, 95],
 };
 const tempMinIdeal = {
   horas: dadosSensorA.horas,
-  temperatura: [18, 18, 18, 18],
-  umidade: [80, 80, 80, 80],
+  temperatura: [18, 18, 18, 18,18, 18, 18, 18],
+  umidade: [80, 80, 80, 80,80, 80, 80, 80],
 };
 
 new Chart(graficoTemp, {
@@ -187,6 +232,8 @@ new Chart(graficoUmidade, {
      
     scales: {
       y: {
+        min: 50,
+        max: 100,
         grid: {
           display: true,
           color: "rgba(255,99,132,0.2)",
