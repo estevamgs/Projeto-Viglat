5a381eb5c5a4b95ba0a85a9315a7a94ba6c4bfd9
estevamgs <gustavo.matos@sptech.school>
2026-06-03 13:45:29 -0300

    Atualização da dashboard

--- VigLat/web-data-viz-main/app.js
+++ VigLat/web-data-viz-main/app.js
@@ -19,8 +19,9 @@ var indexRouter = require("./src/routes/index");
 var usuarioRouter = require("./src/routes/usuarios");
 var avisosRouter = require("./src/routes/avisos");
 var medidasRouter = require("./src/routes/medidas");
-var aquariosRouter = require("./src/routes/aquarios");
+var aquariosRouter = require("./src/routes/dash");
 var empresasRouter = require("./src/routes/empresas");
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -34,6 +35,7 @@ app.use("/avisos", avisosRouter);
 app.use("/medidas", medidasRouter);
 app.use("/aquarios", aquariosRouter);
 app.use("/empresas", empresasRouter);
+app.use("/dash", dashRouter);
 
 app.listen(PORTA_APP, function () {
     console.log(`
---
+++ VigLat/web-data-viz-main/src/routes/dash.js
@@ -0,0 +1,14 @@
+var express = require("express");
+var router = express.Router();
+
+var dashController = require("../controllers/dashController");
+
+router.get("dash/:fazendaId/:idFazenda", function (req, res) {
+  dashController.formarDashboard(req, res);
+});
+
+router.post("/cadastrar", function (req, res) {
+  dashController.cadastrar(req, res);
+})
+
+module.exports = router;
