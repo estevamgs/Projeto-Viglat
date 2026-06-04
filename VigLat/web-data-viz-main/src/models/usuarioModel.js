7d179cc5107bb0f8175ff85b344241abc15d25bb
ViniciusGarban <vinicius.garban@sptech.school>
2026-06-04 18:16:25 -0300

    m,ais rotas

--- VigLat/web-data-viz-main/src/models/usuarioModel.js
+++ VigLat/web-data-viz-main/src/models/usuarioModel.js
@@ -1,20 +1,20 @@
 var database = require("../database/config");
 
-function autenticar(email, senha) {
+function cadastrar(nome, email, senha, empresaId) {
     var instrucaoSql = `
-        SELECT idUsuario, nome, email, senha, empresa_id
-        FROM usuario
-        WHERE email = '${email}' AND senha = '${senha}';
+        INSERT INTO usuario (nome, email, senha, empresa_id)
+        VALUES ('${nome}', '${email}', '${senha}', ${empresaId});
     `;
 
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
 }
 
-function cadastrar(nome, email, senha, empresaId) {
+function autenticar(email, senha) {
     var instrucaoSql = `
-        INSERT INTO usuario (nome, email, senha, empresa_id)
-        VALUES ('${nome}', '${email}', '${senha}', ${empresaId});
+        SELECT idUsuario, nome, email, senha, empresa_id
+        FROM usuario
+        WHERE email = '${email}' AND senha = '${senha}';
     `;
 
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
