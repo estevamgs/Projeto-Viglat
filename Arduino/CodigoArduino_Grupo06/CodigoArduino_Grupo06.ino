8169cfd21c330902c56e060c4fe0a88212882088
Leonardo Moura de Sousa <leonardo.sousa@sptech.school>
2026-06-05 14:21:42 -0300

    Código do Arduino Comentado

--- Arduino/CodigoArduino_Grupo06/CodigoArduino_Grupo06.ino
+++ Arduino/CodigoArduino_Grupo06/CodigoArduino_Grupo06.ino
@@ -1,15 +1,23 @@
+// Importando Bibliotecas Necessárias, certifique-se de que estão instaladas, o nome é DHT11 Library
+
 #include <DHT.h>
 #include <DHT_U.h>
+
+// Definir variáveis
 
 #define TIPO_SENSOR DHT11
 const int PINO_SENSOR_DHT11 = A0;
 
 DHT sensorDHT(PINO_SENSOR_DHT11, TIPO_SENSOR);
 
+// Fazer a inicialização do código, configurar a frequência com que o sensor vai mandar info para o código 
+
 void setup() {
   Serial.begin(9600);
   sensorDHT.begin();
 }
+
+// O código que será rodado e repetido mandando informação para o console
 
 void loop() {
   float umidade = sensorDHT.readHumidity();
