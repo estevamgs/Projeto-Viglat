e8d8cccf7f99a7bf0898da4769436310a02d3292
Leonardo Moura de Sousa <leonardomourass2008@gmail.com>
2026-06-04 22:06:21 -0300

    Ajustando CSS de algumas páginas

--- VigLat/web-data-viz-main/public/Páginas do site/BOBIA/bobIA.js
+++ VigLat/web-data-viz-main/public/Páginas do site/BOBIA/bobIA.js
@@ -1,31 +1,31 @@
-      async function gerarResposta() {
-        const perguntaDiv = document.getElementById("pergunta");
-        const pergunta = perguntaDiv.value;
+async function gerarResposta() {
+  const perguntaDiv = document.getElementById("pergunta");
+  const pergunta = perguntaDiv.value;
 
-        perguntaDiv.value = ``;
+  perguntaDiv.value = ``;
 
-        document.getElementById("resposta").innerHTML += `<span
+  document.getElementById("resposta").innerHTML += `<span
               style="
                 align-self: flex-end;
                 background-color: rgb(94, 191, 236);
                 color: white;
                 padding: 0.5em;
-                border-radius: 0.5em;
+                border-radius: 0.5em;  max-width: 80%;
               "
               >${pergunta}</span
             >`;
 
-        const response = await fetch("/perguntar", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ pergunta }),
-        });
+  const response = await fetch("/perguntar", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ pergunta }),
+  });
 
-        const data = await response.json();
+  const data = await response.json();
 
-        document.getElementById("resposta").innerHTML += `<span
+  document.getElementById("resposta").innerHTML += `<span
               style="
                 align-self: flex-start;
                 background-color: orange;
@@ -34,14 +34,14 @@
               "
               >${data.resultado}</span
             >`;
-      }
+}
 
-      function aparecerChat() {
-        const container = document.getElementById("container");
-        if (container.style.display == "") {
-          container.style.display = "flex";
-        } else {
-          container.style.display = "";
-          document.getElementById("resposta").innerHTML = "";
-        }
-      }
+function aparecerChat() {
+  const container = document.getElementById("container");
+  if (container.style.display == "") {
+    container.style.display = "flex";
+  } else {
+    container.style.display = "";
+    document.getElementById("resposta").innerHTML = "";
+  }
+}
