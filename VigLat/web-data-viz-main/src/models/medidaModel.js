bcc906fae32daeee8ff73424a620e053ed02dfac
Lucas Cordeiro <lucas.cordeiro@sptech.school>
2026-06-06 17:13:05 -0300

    Feature: criando select para buscar ultimasMedidas no database

--- VigLat/web-data-viz-main/src/models/medidaModel.js
+++ VigLat/web-data-viz-main/src/models/medidaModel.js
@@ -2,14 +2,17 @@ var database = require("../database/config");
 
 function buscarUltimasMedidas(idSensor, limite_linhas) {
 
-    var instrucaoSql = `SELECT 
-        dht11_temperatura as temperatura, 
-        dht11_umidade as umidade,
-                        momento,
-                        DATE_FORMAT(momento,'%H:%i:%s') as momento_grafico
-                    FROM medida
-                    WHERE fk_aquario = ${idSensor}
-                    ORDER BY id DESC LIMIT ${limite_linhas}`;
+     var instrucaoSql = `
+        SELECT
+            temperatura,
+            umidade,
+            dt_Hora,
+            DATE_FORMAT(dt_Hora, '%H:%i:%s') AS momento_grafico
+        FROM registro
+        WHERE idSensor = ${idSensor}
+        ORDER BY dt_Hora DESC
+        LIMIT ${limite_linhas};
+    `;
 
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
